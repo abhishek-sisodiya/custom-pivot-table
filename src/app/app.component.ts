d18e4f0888c649db37715fe2af22b54d35959bfb
abhishek-sisodiya <abhisheksisodiya01@gmail.com>
2019-09-19 12:51:26 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+// import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 
 @Component({
@@ -7,8 +7,12 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showPre = false;
+  dropdownList = [];
+  selectedItems = [];
+  dropdownSettings = {};
+
   activeCustomers = [
     'Store',
     'Category',
@@ -16,6 +20,40 @@ export class AppComponent {
   ];
 
   inactiveCustomers = [];
+
+
+  ngOnInit() {
+    this.dropdownList = [
+      { item_id: 1, item_text: '2015' },
+      { item_id: 2, item_text: '2016' },
+      { item_id: 3, item_text: '2017' },
+      { item_id: 4, item_text: '2018' },
+      { item_id: 5, item_text: '2019' }
+    ];
+    this.selectedItems = [
+      // { item_id: 3, item_text: 'Pune' },
+      // { item_id: 4, item_text: 'Navsari' }
+    ];
+    this.dropdownSettings = {
+      singleSelection: false,
+      idField: 'item_id',
+      textField: 'item_text',
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      itemsShowLimit: 3,
+      allowSearchFilter: true
+    };
+  }
+
+  onItemSelect() {
+    console.log(this.selectedItems);
+  }
+  onSelectAll(items: any) {
+    console.log(items);
+  }
+  onItemDeSelect() {
+    console.log(this.selectedItems);
+  }
 
   drop(event: CdkDragDrop<string[]>) {
 
@@ -33,9 +71,9 @@ export class AppComponent {
         event.currentIndex
       );
     }
-    
+
     this.pre = JSON.stringify(this.inactiveCustomers, null, ' ');
-    
+
   }
 
   pre = JSON.stringify(this.inactiveCustomers, null, ' ');
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MarkdownModule } from 'ngx-markdown';
 
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @NgModule({
   imports: [
+    FormsModule, ReactiveFormsModule,
+    NgMultiSelectDropDownModule.forRoot(),
     BrowserModule,
     DragDropModule,
     MarkdownModule.forRoot()
