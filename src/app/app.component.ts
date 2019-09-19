a64a51641640a1c7d38de106a98acdb822ab2509
abhishek-sisodiya <abhisheksisodiya01@gmail.com>
2019-09-19 18:54:53 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 // import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { FormGroup, FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'my-app',
@@ -9,24 +11,26 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) { }
+
   showPre = false;
   dropdownList = [];
   selectedItems = [];
   dropdownSettings = {};
-  from : string;
-  to : string;
-  cities= [];
+  from: string;
+  to: string;
+  cities = [];
   selectedItem1 = [];
   dropdownSettings1 = {};
-  closeDropdownSelection=false;
-  disabled=false;
-
+  closeDropdownSelection = false;
+  disabled = false;
+  data;
   activeCustomers = [
     'Store',
     'Category',
     'Region'
   ];
-
+  namedMonths = ["February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January"];
   inactiveCustomers = [];
   formdata: any;
 
@@ -56,24 +60,35 @@ export class AppComponent implements OnInit {
     this.cities = ['Year', 'Month', 'Quater', 'Week'];
     this.selectedItem1 = [];
     this.dropdownSettings1 = {
-        singleSelection: true,
-        selectAllText: 'Select All',
-        unSelectAllText: 'UnSelect All',
-        allowSearchFilter: true,
-        closeDropDownOnSelection: this.closeDropdownSelection
+      singleSelection: true,
+      selectAllText: 'Select All',
+      unSelectAllText: 'UnSelect All',
+      allowSearchFilter: true,
+      closeDropDownOnSelection: this.closeDropdownSelection
     };
     this.formdata = new FormGroup({
       from: new FormControl(""),
       to: new FormControl("")
-   });
+    });
+
+
+    this.getJSON().subscribe(data => {
+      console.log(data);
+      this.data = data;
+      console.log(this.namedMonths);
+      let arr = [];
+
+      this.monthsBetween(this.data.from_date, this.data.to_date, function (month) { arr.push(this.namedMonths[month]); });
+    });
+
   }
   onItemSelect1(item: any) {
     console.log('onItemSelect', item);
-}
-toggleCloseDropdownSelection() {
-  this.closeDropdownSelection = !this.closeDropdownSelection;
-  this.dropdownSettings1 = Object.assign({}, this.dropdownSettings1,{closeDropDownOnSelection: this.closeDropdownSelection});
-}
+  }
+  toggleCloseDropdownSelection() {
+    this.closeDropdownSelection = !this.closeDropdownSelection;
+    this.dropdownSettings1 = Object.assign({}, this.dropdownSettings1, { closeDropDownOnSelection: this.closeDropdownSelection });
+  }
   onItemSelect() {
     console.log(this.selectedItems);
   }
@@ -106,5 +121,74 @@ toggleCloseDropdownSelection() {
   }
 
   pre = JSON.stringify(this.inactiveCustomers, null, ' ');
+
+
+
+  public getJSON(): Observable<any> {
+    return this.http.get("./assets/data.json");
+  }
+
+  getData() {
+    this.getJSON().subscribe(data => {
+      console.log(data);
+      data = data.dimensions;
+      console.log(data.dimensions);
+    });
+  }
+
+  dateRange(startDate, endDate) {
+    var start = startDate.split('-');
+    var end = endDate.split('-');
+    var startYear = parseInt(start[0]);
+    var endYear = parseInt(end[0]);
+    var dates = [];
+
+    for (var i = startYear; i <= endYear; i++) {
+      var endMonth = i != endYear ? 11 : parseInt(end[1]) - 1;
+      var startMon = i === startYear ? parseInt(start[1]) - 1 : 0;
+      for (var j = startMon; j <= endMonth; j = j > 12 ? j % 12 || 11 : j + 1) {
+        var month = j + 1;
+        var displayMonth = month < 10 ? '0' + month : month;
+        dates.push([i, displayMonth, '01'].join('-'));
+      }
+    }
+    return dates;
+  }
+
+
+  stringToDate(datestring) {
+    var d = new Date(0);
+    d.setHours(2);
+    d.setFullYear(parseInt(datestring.substr(0, 4), 10));
+    d.setMonth(parseInt(datestring.substr(5, 2), 10));
+    d.setDate(parseInt(datestring.substr(8, 2), 10));
+    return d;
+  }
+
+  monthsBetween(from, to, cb) {
+    if (cb === void 0) {
+      cb = function (month) { };
+    }
+    //Convert to date objects
+    var d1 = this.stringToDate(from);
+    var d2 = this.stringToDate(to);
+    //month counter
+    var months = 0;
+    //Call callback function with month
+    cb(d1.getMonth());
+    //While year or month mismatch, reduce by one day
+    while (d2.getFullYear() != d1.getFullYear() || d2.getMonth() != d1.getMonth()) {
+      var oldmonth = d1.getMonth();
+      d1 = new Date(d1.getTime() + 86400000);
+      //if we enter into new month, add to month counter
+      if (oldmonth != d1.getMonth()) {
+        //Call callback function with month
+        cb(d1.getMonth());
+        months++;
+      }
+    }
+    //return month counter as result
+    return months;
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
 
 import { MarkdownModule } from 'ngx-markdown';
 
@@ -11,6 +12,7 @@ import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @NgModule({
   imports: [
+    HttpClientModule,
     FormsModule, ReactiveFormsModule,
     NgMultiSelectDropDownModule.forRoot(),
     BrowserModule,
